3dcfb8af32e9b2bf18a807e651705e0f5ca5525e
zorazky <oswaldo.vazquez@gmail.com>
2019-10-02 19:16:31 -0500

    working with promises

---
+++ app.js
@@ -0,0 +1,21 @@
+const fetchPokemon = () => {
+    const promises = [];
+    for (let i = 1; i <= 150; i++) {
+        const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+        promises.push(fetch(url).then( (res) => res.json()));
+    }
+
+    Promise.all(promises).then( results => {
+        const pokemon = results.map( data => ({
+            name: data.name,
+            id: data.id,
+            image: data.sprites['front_default'],
+            type: data.types.map( type => type.type.name).join(', ')
+        }))
+        console.log(pokemon)
+    })
+           
+        
+};
+
+fetchPokemon();
